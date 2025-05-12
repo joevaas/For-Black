6d9360195a4ee066362b04245eccce2d80a9fa85
joevaas <133934323+joevaas@users.noreply.github.com>
2025-05-12 08:17:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ DB_NAME = os.environ.get("DB_NAME", "Pream")
 
 
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002199934355"))
-FORCE_SUB_CHANNEL_1 = int(os.environ.get("FORCE_SUB_CHANNEL_1", "-1002191966684"))
+FORCE_SUB_CHANNEL_1 = int(os.environ.get("FORCE_SUB_CHANNEL_1", "-1002459100850"))
 
 FORCE_SUB_CHANNEL_2 = int(os.environ.get("FORCE_SUB_CHANNEL_2", "-1002448901304"))
 
